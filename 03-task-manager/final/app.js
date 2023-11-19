c49f2bf30963188af85276f04b1a53447cb99a37
raeanne <raeannedavis.commude@gmail.com>
2023-11-19 15:33:20 +0800

    store api

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -4,13 +4,14 @@ const taskRouter = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notfound = require('./middleware/notfound')
-const port = 5000
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const port = process.env.PORT || 5000
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
         app.listen(port, () => {
-            console.log('Server')
+            console.log(`Server listening on port: ${port}`)
         })
     } catch (error) {
         console.log(error)
@@ -25,7 +26,10 @@ app.use(express.json())
 app.use(express.static('./public'))
 // parse form data
 app.use(express.urlencoded({extended: false}))
-app.use(notfound)
+
+//not found middleware
+// app.use(notfound)
+app.use(errorHandlerMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Hello')
